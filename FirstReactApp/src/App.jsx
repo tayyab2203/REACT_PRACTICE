5d6dd959f828a157c290cde9f9da464749a7b5f0
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-08-04 12:32:41 +0500

    learned how to create component, and how to import and export components

--- FirstReactApp/src/App.jsx
+++ FirstReactApp/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Title from "./title";
+import Description from "./description";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>I am app component</h1>
+      <button>Press Me</button>
+      <p>i am paragraph</p>
+      <h6>below is the title, that is a seperate component but we render title component in our app component</h6>
+      <Title />
+      <h6>below is the description , that is a seperate component but we render description component in our app component</h6>
+      <Description/>
+      <Title />
+      <Description/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ FirstReactApp/src/title.jsx
@@ -0,0 +1,6 @@
+
+function Title() {
+  return <h1>Hello i am title of the app</h1>;
+}
+
+export default Title
